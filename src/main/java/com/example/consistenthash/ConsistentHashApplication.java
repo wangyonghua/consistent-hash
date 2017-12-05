c632b9e8a986d70d0602cd07aa2a70e4969c32e4
wangyonghua <wangyonghua_java@126.com>
2017-12-05 17:49:41 +0800

    add source

---
+++ src/main/java/com/example/consistenthash/ConsistentHashApplication.java
@@ -0,0 +1,12 @@
+package com.example.consistenthash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsistentHashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsistentHashApplication.class, args);
+	}
+}
